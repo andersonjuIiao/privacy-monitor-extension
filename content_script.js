92795a3783b9f33536e5dfafafda22d3ec7c56c2
Anderson Julião <antonioaaj@al.insper.edu.br>
2026-05-09 00:43:23 -0300

    fix: melhora deteccao de fingerprinting com suporte a WebGL2 e AudioContext

--- content_script.js
+++ content_script.js
@@ -3,25 +3,29 @@
 function coletarWebStorage() {
   const items = [];
 
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
-    items.push({
-      tipo: "localStorage",
-      chave: key,
-      tamanho: value ? value.length : 0
-    });
-  }
+  try {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = localStorage.getItem(key);
+      items.push({
+        tipo: "localStorage",
+        chave: key,
+        tamanho: value ? value.length : 0
+      });
+    }
+  } catch (e) {}
 
-  for (let i = 0; i < sessionStorage.length; i++) {
-    const key = sessionStorage.key(i);
-    const value = sessionStorage.getItem(key);
-    items.push({
-      tipo: "sessionStorage",
-      chave: key,
-      tamanho: value ? value.length : 0
-    });
-  }
+  try {
+    for (let i = 0; i < sessionStorage.length; i++) {
+      const key = sessionStorage.key(i);
+      const value = sessionStorage.getItem(key);
+      items.push({
+        tipo: "sessionStorage",
+        chave: key,
+        tamanho: value ? value.length : 0
+      });
+    }
+  } catch (e) {}
 
   return items;
 }
@@ -30,37 +34,73 @@ function injetarDetectoresFingerprinting() {
   const script = document.createElement("script");
   script.textContent = `
     (function() {
-      const origToDataURL = HTMLCanvasElement.prototype.toDataURL;
-      HTMLCanvasElement.prototype.toDataURL = function() {
-        window.postMessage({ tipo: "fingerprint", evento: "Canvas: toDataURL" }, "*");
-        return origToDataURL.apply(this, arguments);
-      };
+      function notificar(evento) {
+        window.postMessage({ tipo: "fingerprint", evento: evento }, "*");
+      }
 
-      const origGetImageData = CanvasRenderingContext2D.prototype.getImageData;
-      CanvasRenderingContext2D.prototype.getImageData = function() {
-        window.postMessage({ tipo: "fingerprint", evento: "Canvas: getImageData" }, "*");
-        return origGetImageData.apply(this, arguments);
-      };
+      try {
+        const origToDataURL = HTMLCanvasElement.prototype.toDataURL;
+        HTMLCanvasElement.prototype.toDataURL = function() {
+          notificar("Canvas: toDataURL");
+          return origToDataURL.apply(this, arguments);
+        };
+      } catch(e) {}
 
-      const origGetParameter = WebGLRenderingContext.prototype.getParameter;
-      WebGLRenderingContext.prototype.getParameter = function(param) {
-        if (param === 37445 || param === 37446) {
-          window.postMessage({ tipo: "fingerprint", evento: "WebGL: getParameter (renderer/vendor)" }, "*");
+      try {
+        const origGetImageData = CanvasRenderingContext2D.prototype.getImageData;
+        CanvasRenderingContext2D.prototype.getImageData = function() {
+          notificar("Canvas: getImageData");
+          return origGetImageData.apply(this, arguments);
+        };
+      } catch(e) {}
+
+      try {
+        const origGetParameterWebGL = WebGLRenderingContext.prototype.getParameter;
+        WebGLRenderingContext.prototype.getParameter = function(param) {
+          if (param === 37445 || param === 37446) {
+            notificar("WebGL: getParameter (renderer/vendor)");
+          }
+          return origGetParameterWebGL.apply(this, arguments);
+        };
+      } catch(e) {}
+
+      try {
+        const origGetParameterWebGL2 = WebGL2RenderingContext.prototype.getParameter;
+        WebGL2RenderingContext.prototype.getParameter = function(param) {
+          if (param === 37445 || param === 37446) {
+            notificar("WebGL2: getParameter (renderer/vendor)");
+          }
+          return origGetParameterWebGL2.apply(this, arguments);
+        };
+      } catch(e) {}
+
+      try {
+        const OrigAudioContext = window.AudioContext || window.webkitAudioContext;
+        if (OrigAudioContext) {
+          const origCreateOscillator = OrigAudioContext.prototype.createOscillator;
+          OrigAudioContext.prototype.createOscillator = function() {
+            notificar("AudioContext: createOscillator");
+            return origCreateOscillator.apply(this, arguments);
+          };
+
+          const origCreateDynamics = OrigAudioContext.prototype.createDynamicsCompressor;
+          OrigAudioContext.prototype.createDynamicsCompressor = function() {
+            notificar("AudioContext: createDynamicsCompressor");
+            return origCreateDynamics.apply(this, arguments);
+          };
         }
-        return origGetParameter.apply(this, arguments);
-      };
+      } catch(e) {}
 
-      const origCreateOscillator = AudioContext.prototype.createOscillator;
-      AudioContext.prototype.createOscillator = function() {
-        window.postMessage({ tipo: "fingerprint", evento: "AudioContext: createOscillator" }, "*");
-        return origCreateOscillator.apply(this, arguments);
-      };
+      try {
+        const origGetContext = HTMLCanvasElement.prototype.getContext;
+        HTMLCanvasElement.prototype.getContext = function(type) {
+          if (type === "webgl" || type === "experimental-webgl" || type === "webgl2") {
+            notificar("WebGL: getContext (" + type + ")");
+          }
+          return origGetContext.apply(this, arguments);
+        };
+      } catch(e) {}
 
-      const origCreateDynamics = AudioContext.prototype.createDynamicsCompressor;
-      AudioContext.prototype.createDynamicsCompressor = function() {
-        window.postMessage({ tipo: "fingerprint", evento: "AudioContext: createDynamicsCompressor" }, "*");
-        return origCreateDynamics.apply(this, arguments);
-      };
     })();
   `;
   document.documentElement.appendChild(script);
